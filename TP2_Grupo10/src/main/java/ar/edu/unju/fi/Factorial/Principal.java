12f2b3d499a399aa155211537e63754571a88c61
fv322 <38471258@fi.unju.edu.ar>
2022-04-15 19:01:56 -0300

    Creación y desarrollo del paquete Factorial para actividad 1.

---
+++ TP2_Grupo10/src/main/java/ar/edu/unju/fi/Factorial/Principal.java
@@ -0,0 +1,38 @@
+package ar.edu.unju.fi.Factorial;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("DETERMINAR EL FACTORIAL DE UN NUMERO INGRESADO.");
+		System.out.println("Ingrese numero: ");
+		Scanner entrada;
+		int numero ;
+		int aux = 1;
+		try {
+			entrada = new Scanner(System.in);
+			numero = entrada.nextInt();
+			if ((numero >= 0) && (numero <= 10)) {
+				for (int i = 1; i <= numero; i++) {
+					aux *= i;
+				}
+
+				System.out.println("El factorial de " + numero + " es " + aux);
+			} else {
+				System.out.println(" El numero ingresado esta fuera del rango permitido");
+			}
+			entrada.close();
+
+		} catch (InputMismatchException e) {
+			System.out.println("Error! Debe ingresar un valor numerico." );
+		}
+		System.out.println("FIN DEL PROGRAMA.");
+	
+	
+
+	}
+
+}
